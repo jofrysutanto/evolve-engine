8a2eb1dbf8a0748d0e46ed9bf129213a637da884
Remo <remo.trueagency.com.au>
2017-05-25 17:26:57 +1000

    Remove Google Analytics from TrueLib

--- src/Utils/UtilityProvider.php
+++ src/Utils/UtilityProvider.php
@@ -24,7 +24,6 @@ class UtilityProvider extends ServiceProvider
         });
 
         $this->app->filter('acf/format_value/type=wysiwyg', 'truelib@formatWysiwygFieldValue', 20, 3);
-        $this->app->action('wp_head', 'truelib@addGoogleAnalytics');
         
         add_filter( 'w3tc_can_print_comment', '__return_false', 10, 1 );
 
