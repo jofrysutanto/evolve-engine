ceb18bf73640f4c2b3f6ff3ebe268d2098262c8d
Phillip McCallum <phillip@trueagency.com.au>
2017-12-07 12:05:20 +0000

    z-index fix

--- src/Utils/AdminStyles.php
+++ src/Utils/AdminStyles.php
@@ -166,7 +166,7 @@ class AdminStyles
                 padding-top: 50px;
             }
             span.acf-recommended-size {
-                background-color: #3498db; color: #fff; position: relative; display: inline-block; z-index: 101;
+                background-color: #3498db; color: #fff; position: relative; display: inline-block; z-index: 10;
                 padding: 0px 0px 0px 30px; margin-bottom: 5px;
                 line-height: 30px; height: 30px; padding-bottom: 0px;
                 -webkit-transition: padding-bottom 0.2s ease; -o-transition: padding-bottom 0.2s ease; transition: padding-bottom 0.2s ease;
