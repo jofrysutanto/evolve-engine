65085f05c9740fe79bdf7acdeacce8b6fba9032d
Jofry <jofry@trueagency.com.au>
2019-04-18 14:18:27 +1000

    Add helpful link

--- src/Post/AbstractPostType.php
+++ src/Post/AbstractPostType.php
@@ -4,12 +4,11 @@ namespace EvolveEngine\Post;
 
 abstract class AbstractPostType
 {
-    
     /**
      * Create new post type, by extending from AbstractPostType
      * and register them as part of config/post-types
      */
-    
+
     /**
      * @var string  The unique key of the custom post type
      */
@@ -56,14 +55,16 @@ abstract class AbstractPostType
 
     /**
      * @var array  List of post type supported Wordpress feature.
+     *
+     * @see https://codex.wordpress.org/Function_Reference/add_post_type_support
      */
     protected $supports = ['title'];
 
     /**
      * Add ACF Tax options, will be accessible using `cpt_{post_type}`
      * e.g. get_field('banner_image', 'cpt_true_post_type')
-     * 
-     * @var boolean 
+     *
+     * @var boolean
      */
     public $hasAcfArchive = true;
 
@@ -75,7 +76,7 @@ abstract class AbstractPostType
         //     'slug'   => 'cpts',
         //     'single' => 'Category',
         //     'plural' => 'Categories',
-        // ]  
+        // ]
     ];
 
     /**
@@ -102,15 +103,15 @@ abstract class AbstractPostType
     {
         $args = [
             'labels' => [
-                'name' => __( $this->pluralName, 'TRUE'),
-                'singular_name' => __( $this->singleName, 'TRUE'),
-                'add_new_item'  => __( 'New ' . $this->singleName, 'TRUE'),
-                'add_new'             => __( 'New ' . $this->singleName, 'TRUE'),
-                'edit_item'           => __( 'Edit ' . $this->singleName, 'TRUE'),
-                'update_item'         => __( 'Update ' . $this->singleName, 'TRUE'),
-                'search_items'        => __( 'Search ' . $this->pluralName, 'TRUE'),
-                'not_found'           => __( 'No ' . $this->pluralName . ' found', 'TRUE'),
-                'not_found_in_trash'  => __( 'No ' . $this->pluralName . ' found in Trash', 'TRUE'),
+                'name'                => __($this->pluralName, 'TRUE'),
+                'singular_name'       => __($this->singleName, 'TRUE'),
+                'add_new_item'        => __('New ' . $this->singleName, 'TRUE'),
+                'add_new'             => __('New ' . $this->singleName, 'TRUE'),
+                'edit_item'           => __('Edit ' . $this->singleName, 'TRUE'),
+                'update_item'         => __('Update ' . $this->singleName, 'TRUE'),
+                'search_items'        => __('Search ' . $this->pluralName, 'TRUE'),
+                'not_found'           => __('No ' . $this->pluralName . ' found', 'TRUE'),
+                'not_found_in_trash'  => __('No ' . $this->pluralName . ' found in Trash', 'TRUE'),
             ],
             'public'              => true,
             'has_archive'         => true,
@@ -120,16 +121,16 @@ abstract class AbstractPostType
             'exclude_from_search' => false,
             'show_in_rest'        => $this->hasApi
         ];
-            
-        if(trim($this->slug) != '') {
+
+        if (trim($this->slug) != '') {
             $args['rewrite'] = ['slug' => $this->slug, 'with_front' => false];
         } else {
             $args['rewrite'] = false;
         }
 
         $args = $this->extendConfigurations($args);
-        
-        register_post_type( $this->id, $args);
+
+        register_post_type($this->id, $args);
 
         $this->createTaxonomy();
 
@@ -158,44 +159,44 @@ abstract class AbstractPostType
     protected function registerTax($taxID, $tax)
     {
         // Register Custom Taxonomy
-        $labels = array(
-            'name'                       => _x( $this->singleName . ' '.$tax['plural'].'', 'Taxonomy General Name', 'text_domain' ),
-            'singular_name'              => _x( $this->singleName . ' '.$tax['single'].'', 'Taxonomy Singular Name', 'text_domain' ),
-        
-            'menu_name'                  => __( $tax['plural'].'', 'text_domain' ),
-        
-            'all_items'                  => __( $this->singleName . ' '.$tax['plural'].'', 'text_domain' ),
-            'parent_item'                => __( 'Parent ' . $this->singleName . ' '.$tax['single'].'', 'text_domain' ),
-            'parent_item_colon'          => __( 'Parent ' . $this->singleName . ' '.$tax['single'].':', 'text_domain' ),
-            'new_item_name'              => __( 'New ' . $this->singleName . ' '.$tax['single'].'', 'text_domain' ),
-            'add_new_item'               => __( 'Add New ' . $this->singleName . ' '.$tax['single'].'', 'text_domain' ),
-            'edit_item'                  => __( 'Edit ' . $this->singleName . ' '.$tax['single'].'', 'text_domain' ),
-            'update_item'                => __( 'Update ' . $this->singleName . ' '.$tax['single'].'', 'text_domain' ),
-        
-            'separate_items_with_commas' => __( 'Separate ' . $this->singleName . ' '.$tax['plural'].' with commas', 'text_domain' ),
-        
-            'search_items'               => __( 'Search ' . $this->singleName . ' '.$tax['plural'].'', 'text_domain' ),
-        
-            'add_or_remove_items'        => __( 'Add or remove ' . $this->singleName . ' '.$tax['plural'].'', 'text_domain' ),
-        
-            'choose_from_most_used'      => __( 'Choose from the most used ' . $this->singleName . ' '.$tax['plural'].'', 'text_domain' ),
-        );
-        
-        $args = array(
+        $labels = [
+            'name'                       => _x($this->singleName . ' ' . $tax['plural'] . '', 'Taxonomy General Name', 'text_domain'),
+            'singular_name'              => _x($this->singleName . ' ' . $tax['single'] . '', 'Taxonomy Singular Name', 'text_domain'),
+
+            'menu_name'                  => __($tax['plural'] . '', 'text_domain'),
+
+            'all_items'                  => __($this->singleName . ' ' . $tax['plural'] . '', 'text_domain'),
+            'parent_item'                => __('Parent ' . $this->singleName . ' ' . $tax['single'] . '', 'text_domain'),
+            'parent_item_colon'          => __('Parent ' . $this->singleName . ' ' . $tax['single'] . ':', 'text_domain'),
+            'new_item_name'              => __('New ' . $this->singleName . ' ' . $tax['single'] . '', 'text_domain'),
+            'add_new_item'               => __('Add New ' . $this->singleName . ' ' . $tax['single'] . '', 'text_domain'),
+            'edit_item'                  => __('Edit ' . $this->singleName . ' ' . $tax['single'] . '', 'text_domain'),
+            'update_item'                => __('Update ' . $this->singleName . ' ' . $tax['single'] . '', 'text_domain'),
+
+            'separate_items_with_commas' => __('Separate ' . $this->singleName . ' ' . $tax['plural'] . ' with commas', 'text_domain'),
+
+            'search_items'               => __('Search ' . $this->singleName . ' ' . $tax['plural'] . '', 'text_domain'),
+
+            'add_or_remove_items'        => __('Add or remove ' . $this->singleName . ' ' . $tax['plural'] . '', 'text_domain'),
+
+            'choose_from_most_used'      => __('Choose from the most used ' . $this->singleName . ' ' . $tax['plural'] . '', 'text_domain'),
+        ];
+
+        $args = [
             'labels'             => $labels,
             'hierarchical'       => true,
             'public'             => true,
             'show_ui'            => true,
             'show_admin_column'  => true,
             'show_in_nav_menus'  => true,
-            'rewrite'            => array('with_front'=> false, 'slug' => $tax['slug']),
+            'rewrite'            => ['with_front'=> false, 'slug' => $tax['slug']],
             'show_tagcloud'      => false,
             'show_in_rest'       => $this->hasApi
-        );
+        ];
 
         $args = $this->extendTaxConfigurations($taxID, $args);
-        
-        register_taxonomy($taxID, $this->id, $args );
+
+        register_taxonomy($taxID, $this->id, $args);
     }
 
     /**
@@ -205,7 +206,7 @@ abstract class AbstractPostType
      *
      * @return string
      */
-    public function getTemplatePath($type = 'single') 
+    public function getTemplatePath($type = 'single')
     {
         $path = false;
         switch ($type) {
@@ -245,7 +246,7 @@ abstract class AbstractPostType
      */
     protected function getIcon()
     {
-        if($this->menuIcon != null) {
+        if ($this->menuIcon != null) {
             $icon = $this->menuIcon;
         } else {
             $icon = asset('img/true-icons/' . $this->menuImage . '.png');
@@ -282,13 +283,13 @@ abstract class AbstractPostType
 
     //
     // Queries
-    // 
+    //
 
     public function whereIn($ids = [])
     {
         return get_posts([
             'post_type' => $this->id,
-            'post__in' => $ids
+            'post__in'  => $ids
         ]);
     }
 
@@ -301,7 +302,7 @@ abstract class AbstractPostType
     {
         return get_posts([
             'post_type' => $this->id,
-            'status' => 'publish'
+            'status'    => 'publish'
         ]);
     }
 
@@ -340,5 +341,4 @@ abstract class AbstractPostType
     {
         return $values;
     }
-    
 }
